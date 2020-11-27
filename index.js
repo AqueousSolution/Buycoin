6bb86cd1b9376e72c59d6b14adbdb4703baa8ec6
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-11-27 08:41:51 +0100

    version1

---
+++ index.js
@@ -0,0 +1,147 @@
+import { query } from "./query.js";
+import { API_KEY } from "./accessToken.js";
+
+const data = { query };
+
+const profileBtn = document.querySelector('.profile-btn')
+const createNewBtn = document.querySelector('.create-btn')
+const createDropdown = document.querySelector("#create-new")
+const profileDropdown = document.querySelector("#profile")
+const profileImage = document.querySelector(".avatar");
+const name = document.querySelector(".side-section__name")
+const userName = document.querySelector(".side-section__username");
+const bio = document.querySelector(".side-section__bio");
+const number = document.querySelector(".number");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const repoList = document.querySelector(".main-section__repolist");
+
+
+
+fetch(`https://api.github.com/graphql`, {
+  method: "POST",
+  headers: {
+    "Authorization": `bearer ${API_KEY}`,
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(data),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("Success:", data.data.viewer);
+
+    removeAllChildNodes(repoList);
+
+    let apiResponse = data.data.viewer;
+    console.log(apiResponse)
+
+    profileImage.setAttribute("src", apiResponse.avatarUrl);
+    name.textContent = apiResponse.name;
+    userName.textContent = apiResponse.login;
+    bio.textContent = apiResponse.bio;
+    number.textContent=apiResponse.repositories.nodes.length
+    //stars.textContent = apiResponse.starredRepositories.totalCount;
+    //badgeValue.textContent = apiResponse.repositories.nodes.length;
+
+    apiResponse.repositories.nodes.forEach(
+      (repo) => addChildNode(repo)
+    );
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+  
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+const addChildNode = (repo) => {
+  var repository = document.createElement("div");
+  repository.className = "repo-content";
+
+  var left = document.createElement("div");
+  left.className = "repo-left";
+
+  var right = document.createElement("div");
+  right.className = "repo-right";
+
+  var h2 = document.createElement("h2");
+  h2.className = "repo-name";
+  var h2TextNode = document.createTextNode(repo.name);
+  h2.appendChild(h2TextNode);
+
+  var p = document.createElement("p");
+  p.className = "repo-summary";
+  var pTextNode = document.createTextNode(repo.description ?? "");
+  p.appendChild(pTextNode);
+
+  var small = document.createElement("small");
+  small.className = "update";
+  var smallTextNode = document.createTextNode('Updated on ' + getDate(repo.updatedAt));
+  small.appendChild(smallTextNode);
+  
+  var tool = document.createElement('span');
+  tool.className = "tool";
+  var toolTextNode = document.createTextNode(repo.languages.nodes[0] == undefined ? "JavaScript": repo.languages.nodes[0].name)
+  tool.appendChild(toolTextNode) 
+
+  
+  var toolColor = document.createElement('div');
+  toolColor.className = "circle";
+  toolColor.style.backgroundColor = repo.languages.nodes[0] == undefined ? "#f1e05a" : repo.languages.nodes[0].color;
+  console.log(toolColor)
+
+
+  var star = document.createElement("img")
+  star.src="./images/star.svg"
+  star.className = "repo-icon";
+
+  var btn = document.createElement("button");
+  btn.className = "star";
+  var btnTextNode = document.createTextNode(` Star`);
+  btn.appendChild(star);
+  btn.appendChild(btnTextNode);
+
+  left.appendChild(h2);
+  left.appendChild(p);
+  left.appendChild(toolColor)
+  left.appendChild(tool)
+  left.appendChild(small);
+  
+
+  right.appendChild(btn);
+
+  repository.appendChild(left);
+  repository.appendChild(right);
+
+  repoList.appendChild(repository);
+}
+
+const getDate = (dateString) => {
+  var targetDate = new Date(dateString).toDateString();
+  return targetDate.split(' ').slice(1,3).join(' ')
+  
+} 
+
+hamburgerMenu.addEventListener('click',()=>{
+dropdown(mobileMenu)
+})
+
+const dropdown = (button) =>{
+  if(button.style.display === "none"){
+    button.style.display = "block";
+  }
+  else{
+    button.style.display = "none";
+  }
+}
+
+profileBtn.addEventListener('click',()=>{
+  dropdown(profileDropdown)
+})
+createNewBtn.addEventListener('click',()=>{
+  dropdown(createDropdown)
+})
---
+++ query.js
@@ -0,0 +1,40 @@
+export const query = `
+  {
+  viewer {
+    login
+    name
+    avatarUrl
+    bio
+    location
+    email
+    status {
+      emoji
+      emojiHTML
+      message
+    }
+    twitterUsername
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    repositories(last: 20, privacy: PUBLIC) {
+      nodes {
+        name
+        description
+        updatedAt
+        languages(first:1) {
+          nodes{
+            color
+            name
+          }
+        }
+      }
+    }
+    starredRepositories {
+      totalCount
+    }
+  }
+}
+`;
